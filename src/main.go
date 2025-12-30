b24f12548daa2b5d41c3630b4075a5ed3e5c2288
alejandrogajadharuoc <agajadhar@uoc.edu>
2025-12-30 18:15:35 +0100

    ultimo cambio pipeline

--- src/main.go
+++ src/main.go
@@ -14,23 +14,37 @@ func main() {
 		port = "8080"
 	}
 
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	mux := http.NewServeMux()
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
 		html := `<!DOCTYPE html>
-        <html>
-        <head><title>Despliegue Seguro</title></head>
-        <body>
-            <h1>Sistema de Despliegue Seguro</h1>
-            <img src="/static/logo.png" alt="Logo" />
-        </body>
-        </html>`
+	<html>
+	<head><title>Despliegue Seguro</title></head>
+	<body>
+		<h1>Sistema de Despliegue Seguro</h1>
+		<img src="/static/logo.png" alt="Logo" />
+		<p>Este es un texto de prueba</p>
+	</body>
+	</html>`
+		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(html))
 	})
 
 	log.Printf("Servidor iniciado en puerto %s", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, loggingMiddleware(mux)))
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc("/error", func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Error interno simulado", http.StatusInternalServerError)
+	})
+
 }
 
 type LogEntry struct {
@@ -51,4 +65,32 @@ func logRequest(r *http.Request, status int) {
 	}
 	jsonLog, _ := json.Marshal(entry)
 	log.Println(string(jsonLog))
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		rr := &responseRecorder{ResponseWriter: w, status: 200}
+		next.ServeHTTP(rr, r)
+
+		logRequest(r, rr.status)
+
+		log.Printf(
+			`{"level":"INFO","message":"request processed","method":"%s","path":"%s","duration_ms":%d}`,
+			r.Method,
+			r.URL.Path,
+			time.Since(start).Milliseconds(),
+		)
+	})
+}
+
+type responseRecorder struct {
+	http.ResponseWriter
+	status int
+}
+
+func (rr *responseRecorder) WriteHeader(code int) {
+	rr.status = code
+	rr.ResponseWriter.WriteHeader(code)
 }
